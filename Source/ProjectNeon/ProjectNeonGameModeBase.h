f0a51d0519f03b0d9a96d274b84ca5d919af1a03
Kelly Carino <mikaela.carino@gmail.com>
2023-09-05 02:25:26 -0700

    initial commit

---
+++ Source/ProjectNeon/ProjectNeonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNEON_API AProjectNeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
